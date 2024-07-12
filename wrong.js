20cf7bb0022c5f438daaeb4ac261cc7d4515a269
boelm <boel_mattsson@hotmail.com>
2024-07-12 14:53:19 +0200

    3 of 13 OK

---
+++ wrong.js
@@ -0,0 +1,15 @@
+//------------PART 2---------------------
+// 1. The logo text of the site has the wrong color. Change it to the correct one.
+const logoRef = document.querySelector('.logo-text');
+logoRef.style.color = 'black';
+
+//2. The alignment of the elements inside the header element are wrong. 
+//Change it to the correct one. Hint, check the flex properties for the correct alignment.
+const headerRef = document.querySelector('header');
+headerRef.style.justifyContent = 'start';
+
+//3. The header has a border at the bottom, but it has the wrong color. Change it do the correct one.
+headerRef.style.borderColor = 'lightgray';
+
+//4. The recipe name is wrong, change it to the correct one.
+
