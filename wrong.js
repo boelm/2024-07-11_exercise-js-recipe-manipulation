8fcb589483026992cdbfdeb0bdea7ac30307bbd6
boelm <boel_mattsson@hotmail.com>
2024-07-15 14:43:31 +0200

    13 of 13 OK :-)

--- wrong.js
+++ wrong.js
@@ -34,9 +34,47 @@ imgRef.setAttribute('src','assets/frozen-cheesecake-slice.jpg');
 
 //8. The background color of the ingredients list container is wrong. Fix it.
 const ingridientsRef = document.querySelector('.ingredients-container');
-console.log(ingridientsRef);
+//console.log(ingridientsRef);
 ingridientsRef.style.backgroundColor = 'rgb(249,249,249)';
 
-//. The ingredients are divided in to two parts, one for the bottom and one for the paste. 
+//9. The ingredients are divided in to two parts, one for the bottom and one for the paste. 
 //In the list of the ingredients to the bottom, there is a text instead of two list items. 
 //Remove the text and add those two list items.
+const ingredientsListBottomRef = document.querySelector('.ingredients-list-bottom');
+let childRef = ingredientsListBottomRef.children[0];
+ingredientsListBottomRef.removeChild(childRef);
+//console.log(ingredientsListBottomRef.children);
+
+const listItem1 = document.createElement('li');
+listItem1.innerHTML = '15 st digestivekex';
+ingredientsListBottomRef.appendChild(listItem1);
+const listItem2 = document.createElement('li');
+listItem2.innerHTML = 'Lite smör';
+ingredientsListBottomRef.appendChild(listItem2);
+
+//10. The third ingredient in the list of ingredients to the paste is wrong. 
+//Change that specific ingredient to the correct one.
+const ingredientsListPasteRef = document.querySelector('.ingredients-list-paste');
+childRef = ingredientsListPasteRef.children[2]; //3rd ingridient
+childRef.innerHTML = '3 tsk vaniljsocker';
+
+//11. There is also a missing ingredient in the list of ingredients to the paste. 
+//Look and see what it is and add that one the the end of the list.
+listItemLast = document.createElement('li');
+listItemLast.innerHTML = '400 g naturell philadelphiaost';
+ingredientsListPasteRef.appendChild(listItemLast);
+
+//12. The text "Instructions" to the left (=right?), beneath the image, has some shadow styling applied to it. 
+//Remove that styling.
+const instructionsH3ContainterRef = document.querySelector('.instructions');
+instructionsH3ContainterRef.classList.remove('shadow');
+
+//13. Two list elements of the list of instructions are incorrect. 
+//Find them and change them to the correct ones.
+//-> li 2 och 9 är fel...
+const instructionListRef = document.querySelector('.instructions-list');
+instructionListRef.children[1].innerHTML = 'Separera ägggulor och äggvitor. Äggvitorna lägger du i en stor bunke, äggulorna i en liten bunke.';
+instructionListRef.children[8].innerHTML = 'Ställ in i frysen över natten.';
+
+
+
